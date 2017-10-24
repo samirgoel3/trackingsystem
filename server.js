fb67974f1c46a94c983d7dd82853b275a50683a2
Samir goel <samirgoel3@gmail.com>
2017-10-24 09:16:44 +0000

    Ceated some web page

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 
-const publicpath = path.join(__dirname,'../public');
-const port = process.env.PORT || 3000 ;
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
+   
+  app.use(express.static(__dirname+'/public'));
 
-app.use(express.static(publicpath));
+// app.use(express.static(publicPath));
+  
+  app.get('/',(req , res)=>{
+      res.send('<h1>Now Working</h1>');
+  });
+  
+  
+  app.get('/about',(req, res) =>{
+      res.send('This is about us page');
+  });
 
-app.listen(port , ()=>{
-    console.log(`Server is up on Port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
