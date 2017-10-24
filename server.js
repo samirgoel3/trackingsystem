7283f86c3629b74685b634495aa4ec58d3c4c880
Samir goel <samirgoel3@gmail.com>
2017-10-24 13:09:39 +0000

    Started over creating design.

--- server.js
+++ server.js
@@ -1,22 +1,59 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
-   
+
+
+  
+  hbs.registerPartials(__dirname+'/views/partials');
+  app.set('view engine','hbs'); 
   app.use(express.static(__dirname+'/public'));
 
-// app.use(express.static(publicPath));
-  
   app.get('/',(req , res)=>{
-      res.send('<h1>Now Working</h1>');
+      res.render('home.hbs' , {
+         date:new Date().getFullYear()
+     });
   });
   
   
+  
+  /////////// about us page
   app.get('/about',(req, res) =>{
-      res.send('This is about us page');
+     res.render('about.hbs' , {
+         date:new Date().getFullYear(),
+         pagename:"About us"
+     });
   });
+  
+  
+  
+  ////////// home page 
+    app.get('/home',(req, res) =>{
+     res.render('home.hbs' , {
+         date:new Date().getFullYear(),
+         pagename:"Home"
+     });
+  });
+  
+  
+  
+    ////////// home page 
+    app.get('/pagetwo',(req, res) =>{
+     res.render('pagetwo.hbs' , {
+         date:new Date().getFullYear(),
+         pagename:"Page two"
+     });
+  });
+  
+  
+  
+  
+  
+  
+  
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
